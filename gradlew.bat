6b263b05848ada9cc37e1a64177cf2b587199a7d
Vovchik48 <karpovvovchik@gmail.com>
2026-03-26 15:16:33 +0300

    Initial commit: Zapret VPN Android app

--- gradlew.bat
+++ gradlew.bat
@@ -1,8 +1,6 @@
 @echo off
-
 set DIRNAME=%~dp0
 set APP_HOME=%DIRNAME%
 set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -Dorg.gradle.appname="gradlew" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
